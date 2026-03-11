1afe13609bf4a0956d00aaebcd1e72cc0ddd378b
mirkholikovmuhammad-boop <mirkholikovmuhammad@gmail.com>
2026-03-11 16:54:11 +0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,23 +1,32 @@
-const CACHE_NAME = "billiard-app-v1";
+const CACHE_NAME = 'billiard-cache-v3';
+const ASSETS = [
+  './',
+  './index.html',
+  './style.css',
+  './script.js',
+  './manifest.json'
+];
 
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll([
-        "./",
-        "index.html",
-        "style.css",
-        "script.js",
-        "icon.png"
-      ]);
-    })
-  );
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
 });
 
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
-  );
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
 });
