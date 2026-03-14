953da235da37d06341cc32e6fe0407c59ef5becf
mirkholikovmuhammad-boop <mirkholikovmuhammad@gmail.com>
2026-03-14 15:29:24 +0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,32 +1,36 @@
-const CACHE_NAME = 'billiard-cache-v3';
+const CACHE_NAME = 'billiard-cache-v10'; // Версия увеличена до 10
 const ASSETS = [
-  './',
-  './index.html',
-  './style.css',
-  './script.js',
-  './manifest.json'
+  './',
+  './index.html',
+  './style.css',
+  './script.js',
+  './manifest.json'
 ];
 
 self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
-  );
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+  // Заставляет новый SW активироваться сразу, не дожидаясь закрытия вкладок
+  self.skipWaiting(); 
 });
 
 self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys().then((keys) => {
-      return Promise.all(
-        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
-      );
-    })
-  );
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+      );
+    })
+  );
+  // Позволяет SW сразу начать контролировать страницу
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
 });
